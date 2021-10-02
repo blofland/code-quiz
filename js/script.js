db3c47666433a71119075637b0ea9619ef1b9061
unknown <boedi15@gmail.com>
2021-10-02 17:40:16 -0600

    added some java

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 const question = document.querySelector('#question')
-const question = document.querySelector('#question')
-const question = document.querySelector('#question')
-const question = document.querySelector('#question')
+const choices = document.querySelector('#question')
+const score = document.querySelector('#question')
+
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
+
+let questions = [
+    {question: "How do you succeed?",
+choice1: "hey",
+choice2: "hi",
+choice3: "nope",
+choice4: "nottoday",
+answer: 2,
+    },
+]
