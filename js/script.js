78830122542e02632a35f8d06acfd8e516ad0ba5
unknown <boedi15@gmail.com>
2021-10-19 19:10:46 -0600

    added

--- js/script.js
+++ js/script.js
@@ -17,3 +17,36 @@ choice4: "nottoday",
 answer: 2,
     },
 ]
+
+const SCORE_POINTS = 1
+const MAX_QUESTIONS = 5
+
+startQuiz = () => {
+    questionCounter = 0
+    score = 0
+    availableQestions = [...questions]
+    getNewQuestion()
+}
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionsCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
+
+        return window.location.assign('end.html')
+    }
+
+    questionCounter++
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS)}%`
+
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+    
+    choices.forEach(choice => {
+        const number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    
+}
